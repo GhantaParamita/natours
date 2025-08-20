79e2516cadb6535f5987cf8ea1394b55af6dae72
Paramita Ghanta <ghantaparamita@gmail.com>
2025-08-20 17:48:01 +0530

    fix reviewRouters typo issue again

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,7 @@ const Review = require("./../models/reviewModel");
 const factory = require("./handlerFactory");
 // const catchAsync = require("./../utils/catchAsync");
 
-exports.satTourUserIds = (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   //Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
