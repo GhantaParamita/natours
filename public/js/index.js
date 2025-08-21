e646137758c1527ce57103cbc8b59c0417a32914
Paramita Ghanta <ghantaparamita@gmail.com>
2025-08-21 19:29:03 +0530

    Implemented stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
+/* eslint-disable */
 import "@babel/polyfill";
 import { displayMap } from "./mapbox";
 import { login, logout } from "./login";
 import { updateSettings } from "./updateSettings";
 import { bookTour } from "./stripe";
+import { showAlert } from "./alerts";
 
 //DOM ELEMENTS
 const mapbox = document.getElementById("map");
@@ -65,3 +67,6 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+const alertMessage = document.querySelector("body").dataset.alert;
+if (alertMessage) showAlert("success", alertMessage, 20);
